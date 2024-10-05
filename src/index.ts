ba73e7aae7ceda0fedc509fc934aa79ca1e8b907
MarcosTenorioDev <marcosvtenoriodev@gmail.com>
2024-10-05 16:06:01 -0300

    Feat: add cors config no frontend url

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import express, { Request, Response } from 'express';
-import { env } from './env';
-import countryRoutes from './routes/country.routes'
+import express, { Request, Response } from "express";
+import { env } from "./env";
+import countryRoutes from "./routes/country.routes";
 import cors from "cors";
 
 const app = express();
-const port = parseInt(env.PORT)
-const host = '0.0.0.0';
+const port = parseInt(env.PORT);
+const host = "0.0.0.0";
 
-app.use(cors());
+app.use(cors({ origin: ["https://develops-today-test-bice.vercel.app", "localhost:5173"] }));
 
 app.use(express.json());
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, Express!');
+app.get("/", (req: Request, res: Response) => {
+	res.send("Hello, Express!");
 });
-app.use("/api", countryRoutes)
+app.use("/api", countryRoutes);
 
 app.listen(port, host, () => {
-  console.log(`Server is running on http://${host}:${port}`);
+	console.log(`Server is running on http://${host}:${port}`);
 });
